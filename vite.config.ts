038e755a09da89f8586f2466023b8788054c43ea
Hiba-Mohamed <hibamoha987@gmail.com>
2025-01-24 15:55:28 -0330

    hosts permissions

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/AnalogAI/", // Change this to match your repository name
   plugins: [react()],
+  preview: {
+    allowedHosts: ["analogai.onrender.com", "localhost"],
+  },
 });
